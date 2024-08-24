e033b5c3699a44e7fca11d506cb20659767a3b70
the-plus-io <hi@the-plus.io>
2024-08-24 11:22:55 +0200

    vite config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from '@vercel/remix/vite';
 
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
@@ -46,6 +47,7 @@ export default defineConfig({
   plugins: [
     remix({
       ignoredRouteFiles: ["**/.*"],
+      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
